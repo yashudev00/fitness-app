58c09e37e2835d8743b69cf1c3bddd49c3c2c26a
yashudev00 <vdyashu09@gmail.com>
2025-08-16 20:57:56 +0530

    written logic in ActivityService

---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,44 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        Activity activity=Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned((request.getCaloriesBurned()))
+                .startTime(request.getStartTime())
+                .additionalMatrics(request.getAdditionalMatrics())
+                .build();
+
+        Activity savedActivity=activityRepository.save(activity);
+        return mapToResponse(activity);
+    }
+
+    private ActivityResponse mapToResponse(Activity activity){
+        ActivityResponse response=new ActivityResponse();
+        response.setId(activity.getId());
+        response.setUserId((activity.getUserId()));
+        response.setType(activity.getType());
+        response.setDuration(activity.getDuration());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMatrics(activity.getAdditionalMatrics());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatedAt(activity.getUpdatedAt());
+        return response;
+    }
+}
