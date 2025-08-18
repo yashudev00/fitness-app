0298a8cfe45a4020c8e363bae9b9cc94b9722316
yashudev00 <vdyashu09@gmail.com>
2025-08-18 21:56:22 +0530

    used rest clients(web client) for services interactions and done some configurations

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -16,8 +16,13 @@ import java.util.stream.Collectors;
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
+    private final UserValidationService userValidationService;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
+        boolean isValidUser=userValidationService.validateUser(request.getUserId());
+        if(!isValidUser){
+            throw new RuntimeException("Invalid User: "+request.getUserId());
+        }
         Activity activity=Activity.builder()
                 .userId(request.getUserId())
                 .type(request.getType())
