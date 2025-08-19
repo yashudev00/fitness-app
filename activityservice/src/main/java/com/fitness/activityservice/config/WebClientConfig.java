2e6d206715f67d0003eb916b90a304642c3f8149
yashudev00 <vdyashu09@gmail.com>
2025-08-19 06:31:17 +0530

    add few logs for validations

--- activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
@@ -7,12 +7,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder(){
         return WebClient.builder();
     }
 
+    @Bean
     public WebClient userServiceWebClient(WebClient.Builder weClientBuilder){
         return webClientBuilder()
                 .baseUrl("http://USER-SERVICE")
--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -5,10 +5,12 @@ import com.fitness.userservice.dto.UserResponse;
 import com.fitness.userservice.model.User;
 import com.fitness.userservice.repository.UserRepository;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -52,6 +54,7 @@ public class UserService {
     }
 
     public Boolean existByUserId(String userId) {
+        log.info("Calling User Validation API for userId : {}",userId);
         return repository.existsById(userId);
     }
 }
